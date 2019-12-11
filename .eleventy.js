890cf9101c5497253108105bf6eb84bd814296f1
koopReynders <mail@kopo.nl>
2019-12-11 14:58:39 +0100

    ignore de demo please

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets');
-};
-module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('demo');
 };
