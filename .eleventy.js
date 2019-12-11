145b7b6e80a0c517cd1c9b931f10e15e77aeca7d
koopReynders <mail@kopo.nl>
2019-12-11 14:51:13 +0100

    demo file niet meenemen aub

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets');
 };
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('demo');
+};
