d38e7d754de6f3fb69ce7fb817629519f15b1914
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-11 17:38:57 +0000

    Deploy fdnd.nl to github.com/adfrontend/fdnd.nl.git:master

--- demo/1/script.js
+++ demo/1/script.js
@@ -1,9 +1,9 @@
 var link = document.querySelector('#hackaton_video');
 var video = document.querySelector('video');
 var pix = document.querySelector('img[src="img/hackatonweb6.jpg"]');
-console.log("link", link);
-console.log("video", video);
-console.log("pix", pix);
+// console.log("link", link);
+// console.log("video", video);
+// console.log("pix", pix);
 
 /* VIDEO ***
   Croppen,Trimmen, exporteren https://online-video-cutter.com
@@ -17,3 +17,27 @@ link.addEventListener("click",function(){
     //video.src="img/hackaton.mp4";
   }
 );
+
+/* INTERSECTION OBSERVER ****
+  MDN https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
+  https://alligator.io/js/intersection-observer
+*/
+
+const navigatie = document.querySelector('nav');
+const header = document.querySelector('header');
+
+observer = new IntersectionObserver((entry, observer) => {
+  // console.log('entry:', entry);
+  // console.log('observer:', observer);
+  // console.log("entry.isIntersecting",entry[0].isIntersecting);
+
+  if (entry[0].isIntersecting) {
+    // console.log('in the view');
+    navigatie.classList.remove('show');
+  } else {
+    // console.log('out of view');
+    navigatie.classList.add('show');
+  }
+
+});
+observer.observe(header);
