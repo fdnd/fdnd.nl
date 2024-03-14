c9b0b3f1f73a874c0fdb04a5fb47418b5fbf2e71
Cyd Stumpel <info@cydstumpel.nl>
2024-03-14 13:36:11 +0100

    animate all the things

--- main.js
+++ main.js
@@ -13,3 +13,24 @@ if (menuTrigger) {
     })
   }
 }
+
+let radiiVal = 0.5;
+let maxRadius = window.innerWidth > 750 ? 1.5 : 1;
+
+// 🤡
+const lerp = (start, end, amt) => {
+  return (1-amt)*start+amt*end
+}
+
+const animateRadiiIn = () => {
+  radiiVal = radiiVal + lerp(0, maxRadius - 0.5, 0.02)
+  if (radiiVal < maxRadius) {
+    document.querySelector('body').style.setProperty('--radius', `${radiiVal}rem`);
+
+  } else {
+    return;
+  }
+  window.requestAnimationFrame(animateRadiiIn)
+}
+
+window.requestAnimationFrame(animateRadiiIn)
